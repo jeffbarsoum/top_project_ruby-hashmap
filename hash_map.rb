d4cbf36996e1291f01ec55c1f9ae14c0c0a6bcd3
jeffbarsoum <143226227+jeffbarsoum@users.noreply.github.com>
2024-07-04 23:34:31 -0400

    Use hash key function developed in lessons

--- hash_map.rb
+++ hash_map.rb
@@ -14,7 +14,14 @@ class HashMap
   end
 
   # takes a key and produces a hash code with it.
-  def hash(key); end
+  def hash(key)
+    hash_code = 0
+    prime_number = 31
+
+    key.each_char { |char| hash_code = prime_number * hash_code + char.ord }
+
+    hash_code
+  end
 
   # takes two arguments, the first is a key and the second is a value that is assigned to this key.
   # if a key already exists, then the old value is overwritten or we can say that we update the key’s value.
