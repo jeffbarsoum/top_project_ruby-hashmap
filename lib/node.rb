c3cd7313cecf99b989142bbac3974ffb60a36762
jeffbarsoum <143226227+jeffbarsoum@users.noreply.github.com>
2024-07-06 09:53:53 -0400

    Added value to default Node class (moving away from Node subclasses, not necessary)

--- lib/node.rb
+++ lib/node.rb
@@ -3,9 +3,10 @@
 # Parent Node class, for Hashes and LinkedLists, keys and values are added by children
 class Node
   # setters and getters for "value" and "next_node"
-  attr_accessor :next_node
+  attr_accessor :value, :next_node
 
-  def initialize(nxt_nd = nil)
+  def initialize(value = nil, nxt_nd = nil)
+    self.value = value
     self.next_node = nxt_nd
   end
 
