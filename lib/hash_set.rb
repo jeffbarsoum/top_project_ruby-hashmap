ea3d708b0e6596bf371dda6f689da77194baae6b
jeffbarsoum <143226227+jeffbarsoum@users.noreply.github.com>
2024-07-07 08:23:26 -0400

    Most work is done by parent Hash class now, may remove

--- lib/hash_set.rb
+++ lib/hash_set.rb
@@ -5,17 +5,4 @@ require 'linked_list'
 
 # Generates unique hashes for keys and stores them in buckets
 class HashSet < Hash
-  def initialize
-    super
-    self.buckets = []
-    capacity.times { |bucket_index| @buckets[bucket_index] = LinkedList.new }
-    puts "buckets after initialization: \n#{buckets}"
-  end
-
-  # takes two arguments, the first is a key and the second is a value that is assigned to this key.
-  # if a key already exists, then the old value is overwritten or we can say that we update the key’s value.
-  def set(key)
-    bucket(key).append(key)
-    expand
-  end
 end
