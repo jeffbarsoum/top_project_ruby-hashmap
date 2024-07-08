94a4a0931d69c7d3e7efaedd1e2e55440722d315
jeffbarsoum <143226227+jeffbarsoum@users.noreply.github.com>
2024-07-07 21:14:30 -0400

    Add child method to HashSet class

--- lib/hash_set.rb
+++ lib/hash_set.rb
@@ -1,8 +1,16 @@
 # frozen_string_literal: true
 
 require 'hash'
-require 'linked_list'
 
 # Generates unique hashes for keys and stores them in buckets
 class HashSet < Hash
+  # forcing child method to only accept a key and not a value
+  def set(key) # rubocop:disable Lint/UselessMethodDefinition
+    super(key)
+  end
+
+  # returns an array containing all the keys inside the hash map.
+  def keys
+    nodes.map { |node| node.value[:key] }.compact
+  end
 end
