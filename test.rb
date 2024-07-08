698e6c3366a89cf13f02efe8149e1c8d10cecd06
jeffbarsoum <143226227+jeffbarsoum@users.noreply.github.com>
2024-07-07 21:14:53 -0400

    Added HashSet tests

--- test.rb
+++ test.rb
@@ -3,6 +3,7 @@
 $LOAD_PATH.unshift "#{File.dirname(__FILE__)}/lib"
 
 require 'hash_map'
+require 'hash_set'
 
 test = HashMap.new
 
@@ -63,3 +64,43 @@ test.set('lion', 'golden')
 puts "keys: #{test.keys}"
 puts "values: #{test.values}"
 puts "entries: #{test.entries}"
+
+puts "#################################################\n\n\nTEST HashSet"
+test2 = HashSet.new
+test2.set('apple')
+test2.set('banana')
+test2.set('carrot')
+test2.set('dog')
+test2.set('elephant')
+test2.set('frog')
+test2.set('grape')
+test2.set('hat')
+test2.set('ice cream')
+test2.set('jacket')
+test2.set('kite')
+test2.set('lion')
+
+puts 'this should only over-write the existing values of your nodes and not add new ones.'
+test2.set('hat')
+test2.set('ice cream')
+test2.set('jacket')
+test2.set('kite')
+
+puts 'populate hash map with the last node below'
+puts 'capacity of hash map should drop well below your load factor'
+test2.set('moon')
+
+puts 'Test the other methods of your hash sets'
+puts "get(ice cream): #{test2.get('ice cream')}"
+puts "has?(hat): #{test2.has?('hat')}"
+puts "remove(jacket): #{test2.remove('jacket')}"
+puts "length: #{test2.length}"
+puts "clear: #{test2.clear}"
+test2.set('hat')
+test2.set('ice cream')
+test2.set('jacket')
+test2.set('kite')
+puts "keys: #{test2.keys}"
+
+puts 'The below entry should throw an error:'
+test2.set('lion', 'golden')
