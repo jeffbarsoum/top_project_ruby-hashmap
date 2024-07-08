508f97df9b0618d2f5795469fdbe070bd34f296f
jeffbarsoum <143226227+jeffbarsoum@users.noreply.github.com>
2024-07-08 07:42:31 -0400

    Code cleanup and reorganization

--- lib/hash.rb
+++ lib/hash.rb
@@ -2,32 +2,19 @@
 
 require 'linked_list'
 
-# This master class will be used for both HashSet and HashMap,
-# since they share a lot of the same functionality
+# Parent Hash class
 #
 # Note that both the HashSet and HashMap classes are short,
 # because most of the share functionality (and code) lives in the
 # parent Hash class
-class Hash
-  attr_accessor :capacity, :load_factor, :growth_factor, :buckets
-
+class Hash # rubocop:disable Metrics/ClassLength
   def initialize
-    self.capacity = 16
-    self.load_factor = 0.75
-    self.growth_factor = 2
-    self.buckets = []
+    @capacity = 16
+    @load_factor = 0.75
+    @growth_factor = 2
+    @buckets = []
     capacity.times { |bucket_index| @buckets[bucket_index] = nil }
     puts "buckets after initialization: \n#{buckets}"
-  end
-
-  # takes a key and produces a hash code with it.
-  def hash(key)
-    hash_code = 0
-    prime_number = 31
-
-    key.each_char { |char| hash_code = prime_number * hash_code + char.ord }
-
-    hash_code
   end
 
   def set(key, value = nil)
@@ -35,7 +22,7 @@ class Hash
     return overwritten if overwritten
 
     hash = hash(key)
-    bucket = bucket(hash)
+    bucket = buckets[bucket_index(hash)]
     bucket ||= LinkedList.new
     val = { key:, value: }
     bucket.append(hash, **val)
@@ -45,82 +32,26 @@ class Hash
     expand if check_capacity
   end
 
-  def overwrite(key, value)
-    found = find(key)
-    if found
-      node = buckets[found[:buckets_index]].at(found[:bucket_index])
-      puts "Overwite [ #{node.value[:key]}, #{node.value[:value]} ] with [ #{key}, #{value} ]"
-      node.value[:value] = value
-      puts "Overwitten value: [ #{node.value[:key]}, #{node.value[:value]} ]"
-      return { key: node.value[:key], value: node.value[:value] }
-    end
-    false
-  end
-
-  # Checks if our bucket is at load_factor% capacity,
-  # and increases capacity by growth_factor% if it does
-  def expand
-    is_full = check_capacity
-    return false unless is_full
-
-    self.capacity += is_full
-    new_buckets = Array.new(capacity, nil)
-    nodes.each do |node|
-      bucket_index = bucket_index(node.hash)
-      new_buckets[bucket_index] = LinkedList.new
-      new_buckets[bucket_index].append(node.hash, **node.value)
-    end
-    puts "buckets expanded: \n#{new_buckets}"
-    self.buckets = new_buckets
-  end
-
-  def linked_list
-    cls_name = self.class.name.gsub('Hash', 'LinkedList')
-    Object.const_get(cls_name)
-  end
-
-  def check_capacity
-    growth_threshold = (capacity * load_factor).ceil
-    key_count = keys.length
-    additional_buckets = key_count > growth_threshold ? capacity * (growth_factor - 1) : 0
-
-    return false if additional_buckets.zero?
-
-    additional_buckets
-  end
-
-  # takes one argument as a key and returns the value that is assigned to this key. If key is not found, return nil.
+  # takes one argument as a key and returns the value that is assigned to this key.
+  # If key is not found, return nil.
   def get(value, type = :key)
-    found = find(value, type)
-    return buckets[found[:buckets_index]].at(found[:bucket_index]) if found
-
-    false
+    node = get_node(value, type)
+    node&.value
   end
 
-  # checks to make sure the index to reference the bucket is for a bucket that exists
-  # per the assignment, an error is raised if it's not
-  def bucket_index(hash)
-    hash % capacity
-  end
-
-  def bucket(hash)
-    puts "bucket(#{hash}: bucket_index(#{hash}) = #{bucket_index(hash)}"
-    buckets[bucket_index(hash)]
-  end
-
-  # takes a key as an argument and returns true or false based on whether or not the key is in the hash map.
+  # takes a key as an argument and returns true or false
+  # based on whether or not the key is in the hash map.
   def has?(value, type = :key)
     return true if find(value, type)
 
     false
   end
 
+  # find node based on key or value, default is key
   def find(value, type = :key)
     found = nil
     buckets.length.times do |i|
-      raise IndexError if i.negative? || i >= @buckets.length
-
-      bucket = buckets[i]
+      bucket = buckets[check_index(i)]
       next unless bucket
 
       found = bucket.find(value, type)
@@ -136,13 +67,7 @@ class Hash
     found = find(value, type)
     return buckets[found[:buckets_index]].remove_at(found[:bucket_index]) if found
 
-    false
-  end
-
-  def check_index(index)
-    raise IndexError if index.negative? || index >= @buckets.length
-
-    index
+    nil
   end
 
   # returns the number of stored keys in the hash map.
@@ -168,5 +93,83 @@ class Hash
       end
     end
     nodes_array
+  end
+
+  #########################################
+  # Private Methods
+  #########################################
+  private
+
+  attr_reader :capacity, :load_factor, :growth_factor, :buckets
+
+  def check_index(index)
+    raise IndexError if index.negative? || index >= @buckets.length
+
+    index
+  end
+
+  # checks to make sure the index to reference the bucket is for a bucket that exists
+  # per the assignment, an error is raised if it's not
+  def bucket_index(hash)
+    hash % capacity
+  end
+
+  # takes a key and produces a hash code with it.
+  def hash(key)
+    hash_code = 0
+    prime_number = 31
+
+    key.each_char { |char| hash_code = prime_number * hash_code + char.ord }
+
+    hash_code
+  end
+
+  # check if key exists and overwrite value if it does
+  def overwrite(key, value) # rubocop:disable Metrics/AbcSize
+    node = get_node(key)
+    return false unless node
+
+    puts "Overwite [ #{node.value[:key]}, #{node.value[:value]} ] with [ #{key}, #{value} ]"
+    node.value[:value] = value
+    puts "Overwitten value: [ #{node.value[:key]}, #{node.value[:value]} ]"
+    { key: node.value[:key], value: node.value[:value] }
+  end
+
+  # Checks if our bucket is at load_factor% capacity,
+  # and increases capacity by growth_factor% if it does
+  def expand
+    is_full = check_capacity
+    return false unless is_full
+
+    @capacity += is_full
+    @buckets = regen_buckets
+    puts "buckets expanded: \n#{buckets}"
+  end
+
+  def check_capacity
+    growth_threshold = (capacity * load_factor).ceil
+    key_count = keys.length
+    additional_buckets = key_count > growth_threshold ? capacity * (growth_factor - 1) : 0
+
+    return false if additional_buckets.zero?
+
+    additional_buckets
+  end
+
+  def regen_buckets
+    new_buckets = Array.new(capacity, nil)
+    nodes.each do |node|
+      new_buckets[bucket_index(node.hash)] ||= LinkedList.new
+      new_buckets[bucket_index(node.hash)].append(node.hash, **node.value)
+    end
+    new_buckets
+  end
+
+  # get node by either key or value
+  def get_node(value, type = :key)
+    found = find(value, type)
+    return buckets[found[:buckets_index]].at(found[:bucket_index]) if found
+
+    nil
   end
 end
